baec569138d16582014db867e5277808393df32b
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-02-27 15:20:19 +1100

    Adding tree sprite and Creating Overlap script for tree
    Adding tree sprite and Creating Overlap script for tree

--- GameStudio1W1/Assets/helicopter.cs
+++ GameStudio1W1/Assets/helicopter.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class helicopter : MonoBehaviour
 {
     private float MoveSpeed = 6f;
+    Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
         
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
