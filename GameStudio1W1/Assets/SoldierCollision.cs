1c8560499e985d2b6c595ee5f92873d368fc5d21
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-02-28 17:44:06 +1100

    adding pick up sounds when soldier is taken
    adding pick up sounds when soldier is taken

--- GameStudio1W1/Assets/SoldierCollision.cs
+++ GameStudio1W1/Assets/SoldierCollision.cs
@@ -20,7 +20,8 @@ public class SoldierCollision : MonoBehaviour
     int score = 0;
     int scorevalue = 200;
 
-    //Soldier aleady secured 
+    public AudioClip AudioClip;
+    public AudioSource AudioSource;
 
     // Start is called before the first frame update
     public void OnTriggerEnter2D(Collider2D other)
@@ -29,7 +30,8 @@ public class SoldierCollision : MonoBehaviour
         {
             
             Destroy(other.gameObject);
-
+            AudioSource.clip = AudioClip;
+            AudioSource.Play();
             //*************
             pickup += value;
             Soldiertext.text = "Pick up Soldier:" + pickup.ToString();
