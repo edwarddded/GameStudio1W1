461d4cd985fd67882fb08c6d625c3ba86ceedce2
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-02-28 18:00:06 +1100

    Update Helicopter.cs and change the Helicopter direction way when it is moving

--- GameStudio1W1/Assets/helicopter.cs
+++ GameStudio1W1/Assets/helicopter.cs
@@ -7,6 +7,7 @@ public class helicopter : MonoBehaviour
 {
     private float MoveSpeed = 6f;
     Rigidbody2D rb2d;
+    public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,17 @@ public class helicopter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         float translation = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
         float rotation = Input.GetAxis("Horizontal") * MoveSpeed *Time.deltaTime;
-
+        if (rotation <0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
         transform.Translate(rotation, 0, 0);
         transform.Translate(0, translation, 0);
         if (Input.GetKey(KeyCode.R))
